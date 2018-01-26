3a3b5c8835c4e39e7333d3900962c9bd834ff2b7
pousspouss <hugo.pousseur@gmail.com>
2018-01-26 15:51:03 +0100

    Update site

--- public/index.js
+++ public/index.js
@@ -7,15 +7,18 @@
     const div = document.createElement('div');
     const template = actors.map(actor => {
       return `
-        <div class="actor">
-          <span>${actor.who}</span>
-          <span>${actor.type}</span>
-          <span>${actor.amount}</span>
+        <div class="actor row">
+          <span class="medium-3 columns bill_string">${actor.who}</span>
+          <span class="medium-3 columns bill_string">${actor.type}</span>
+          <span class="medium-3 columns bill">${actor.amount}</span>
         </div>
+
       `;
     }).join('');
-
-    div.innerHTML = template;
+    const header = '<div class="row text-center large" id="bill_header_bill">Bill</div>'
+    const industry = '<div class="medium-6 columns row large text-left">Camtar Industry</div>'
+    const date = '<div class="medium-6 columns row large text-left">The 22/02/2018 at 1:00 am</div>'
+    div.innerHTML = "<hr>"+header+"<hr>"+industry+date+"<hr>"+template+"<hr>";
     fragment.appendChild(div);
     document.querySelector('#actors').innerHTML = '';
     document.querySelector('#actors').appendChild(fragment);
